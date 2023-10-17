7ed8e898574d5b3fded616bb73bd8c667717e1f4
Franz Alarcon <franzalarconv@gmail.com>
2023-10-17 01:08:27 -0700

    Change tab size from 2 to 4

--- src/pages/Discussion.tsx
+++ src/pages/Discussion.tsx
@@ -6,35 +6,35 @@ import useFetchBackend from '../hooks/useFetchBackend';
 import { FetchMethod } from '../types/BackendFetchInfo';
 
 export default function Discussion() {
-  const {id} = useParams();
-  const [discussionInfo, discussionError, discussionLoading] = useFetchBackend({
-    route: `/discussion/${id}`,
-    method: FetchMethod.Get
-  });
-  const [quibbleList, quibblesError, quibblesLoading] = useFetchBackend({
-    route: `/discussion/${id}/quibbles`,
-    method: FetchMethod.Get,
-    sendCookies: true
-  });
+    const {id} = useParams();
+    const [discussionInfo, discussionError, discussionLoading] = useFetchBackend({
+        route: `/discussion/${id}`,
+        method: FetchMethod.Get
+    });
+    const [quibbleList, quibblesError, quibblesLoading] = useFetchBackend({
+        route: `/discussion/${id}/quibbles`,
+        method: FetchMethod.Get,
+        sendCookies: true
+    });
 
-  if (discussionLoading) {
+    if (discussionLoading) {
+        return (
+        <h1>Discussion Loading</h1>
+        );
+    }
+
+    if (discussionError) {
+        return (
+        <h1>Could Not Access Discussion</h1>
+        );
+    }
+
     return (
-      <h1>Discussion Loading</h1>
+        <>
+        <h1>Discussion Page</h1>
+        <DiscussionHead discussionInfo={discussionInfo}/>
+        <QuibbleEntryBox discussionId={id}/>
+        {quibbleList && <QuibbleList quibbles={quibbleList.quibbles}/>}
+        </>
     );
-  }
-
-  if (discussionError) {
-    return (
-      <h1>Could Not Access Discussion</h1>
-    );
-  }
-
-  return (
-    <>
-    <h1>Discussion Page</h1>
-    <DiscussionHead discussionInfo={discussionInfo}/>
-    <QuibbleEntryBox discussionId={id}/>
-    {quibbleList && <QuibbleList quibbles={quibbleList.quibbles}/>}
-    </>
-  );
 }
