b146d4ad55157025a07a06e8931cc96608fcba87
Franz Alarcon <franzalarconv@gmail.com>
2023-12-06 22:15:53 -0800

    Add text box styles

--- src/features/styles/index.ts
+++ src/features/styles/index.ts
@@ -1,3 +1,5 @@
+export * from './components/ShortTextBox'
+export * from './components/WideTextBox'
 export * from './components/SectionHeader'
 export * from './components/MainContentRegion'
 export * from './css/baseLargeButton'
